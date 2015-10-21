b5b7973702189d51d48423fedbbc4fbcf4c6d39e
Chris Park <cpar137@aucklanduni.ac.nz>
2015-10-21 16:18:40 +1300

    Update clean.R

--- clean.R
+++ clean.R
@@ -238,7 +238,7 @@ clean.data =
         function(data, vars) {
             ##  Extract data and variable tables.
             dat.tab = get.table(table = data)
-            var.tab = read.csv(vars)
+            var.tab = as.matrix(read.csv(vars))
 
             ##  Variable handling.
             var.col = c("variable", "question", "unit", "type", "category")
